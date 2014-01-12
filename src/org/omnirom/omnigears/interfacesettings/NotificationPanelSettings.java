83af1e7482cb8225da41af5c9dd153e2c32d8e4a
Pierre Gros <super.mamie@gmail.com>
2014-01-12 14:30:38 +0100

    [1/2] OmniGears: notification drawer: custom clock & calendar shortcut
    Base: https://gerrit.omnirom.org/#/c/4811/
    
    PS10:
    added AppSelectListPreference and use for clock and calendar
    
    Change-Id: I68b9a8bbb84042cbf4bc448cfc30599132f03256

--- src/org/omnirom/omnigears/interfacesettings/NotificationPanelSettings.java
+++ src/org/omnirom/omnigears/interfacesettings/NotificationPanelSettings.java
@@ -18,19 +18,25 @@
 
 package org.omnirom.omnigears.interfacesettings;
 
-import com.android.settings.SettingsPreferenceFragment;
-import com.android.settings.R;
-
+import android.app.Activity;
 import android.content.ContentResolver;
-import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.os.Bundle;
 import android.preference.CheckBoxPreference;
 import android.preference.Preference;
-import android.preference.PreferenceGroup;
 import android.preference.PreferenceScreen;
 import android.provider.Settings;
-import android.provider.Settings.SettingNotFoundException;
 import android.util.Log;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.quicklaunch.BookmarkPicker;
+
+import java.net.URISyntaxException;
+
+import org.omnirom.omnigears.preference.AppSelectListPreference;
 
 public class NotificationPanelSettings extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
@@ -38,9 +44,13 @@ public class NotificationPanelSettings extends SettingsPreferenceFragment implem
 
     private static final String STATUS_BAR_CUSTOM_HEADER = "custom_status_bar_header";
     private static final String QUICK_SWIPE = "quick_swipe";
+    private static final String CLOCK_SHORTCUT = "clock_shortcut";
+    private static final String CALENDAR_SHORTCUT = "calendar_shortcut";
 
     private CheckBoxPreference mStatusBarCustomHeader;
     private CheckBoxPreference mQuickSwipe;
+    private AppSelectListPreference mClockShortcut;
+    private AppSelectListPreference mCalendarShortcut;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -59,11 +69,14 @@ public class NotificationPanelSettings extends SettingsPreferenceFragment implem
         mQuickSwipe.setChecked(Settings.System.getInt(resolver,
             Settings.System.QUICK_SWIPE, 1) == 1);
         mQuickSwipe.setOnPreferenceChangeListener(this);
-    }
 
-    @Override
-    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
-        return true;
+        mClockShortcut = (AppSelectListPreference)prefSet.findPreference(CLOCK_SHORTCUT);
+        mClockShortcut.setOnPreferenceChangeListener(this);
+
+        mCalendarShortcut = (AppSelectListPreference)prefSet.findPreference(CALENDAR_SHORTCUT);
+        mCalendarShortcut.setOnPreferenceChangeListener(this);
+
+        updateClockCalendarSummary();
     }
 
     public boolean onPreferenceChange(Preference preference, Object objValue) {
@@ -76,9 +89,60 @@ public class NotificationPanelSettings extends SettingsPreferenceFragment implem
             boolean value = (Boolean) objValue;
             Settings.System.putInt(resolver,
                 Settings.System.QUICK_SWIPE, value ? 1 : 0);
+        } else if (preference == mClockShortcut) {
+            String value = (String) objValue;
+            // a value of null means to use the default
+            Settings.System.putString(resolver, Settings.System.CLOCK_SHORTCUT, value);
+            updateClockCalendarSummary();
+        } else if (preference == mCalendarShortcut) {
+            String value = (String) objValue;
+            // a value of null means to use the default
+            Settings.System.putString(resolver, Settings.System.CALENDAR_SHORTCUT, value);
+            updateClockCalendarSummary();
         } else {
             return false;
         }
         return true;
+    }
+
+    private void updateClockCalendarSummary() {
+        final PackageManager packageManager = getPackageManager();
+
+        mClockShortcut.setSummary(getResources().getString(R.string.default_shortcut));
+        mCalendarShortcut.setSummary(getResources().getString(R.string.default_shortcut));
+
+        String clockShortcutIntentUri = Settings.System.getString(getContentResolver(), Settings.System.CLOCK_SHORTCUT);
+        if (clockShortcutIntentUri != null) {
+            Intent clockShortcutIntent = null;
+            try {
+                clockShortcutIntent = Intent.parseUri(clockShortcutIntentUri, 0);
+            } catch (URISyntaxException e) {
+                clockShortcutIntent = null;
+            }
+
+            if(clockShortcutIntent != null) {
+                ResolveInfo info = packageManager.resolveActivity(clockShortcutIntent, 0);
+                if (info != null) {
+                    mClockShortcut.setSummary(info.loadLabel(packageManager));
+                }
+            }
+        }
+
+        String calendarShortcutIntentUri = Settings.System.getString(getContentResolver(), Settings.System.CALENDAR_SHORTCUT);
+        if (calendarShortcutIntentUri != null) {
+            Intent calendarShortcutIntent = null;
+            try {
+                calendarShortcutIntent = Intent.parseUri(calendarShortcutIntentUri, 0);
+            } catch (URISyntaxException e) {
+                calendarShortcutIntent = null;
+            }
+
+            if(calendarShortcutIntent != null) {
+                ResolveInfo info = packageManager.resolveActivity(calendarShortcutIntent, 0);
+                if (info != null) {
+                    mCalendarShortcut.setSummary(info.loadLabel(packageManager));
+                }
+            }
+        }
     }
 }
