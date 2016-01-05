5678d0afe93fd03fe661c30778a338b5847ce19c
Christian Oder <myself5@carbonrom.org>
2016-01-06 00:59:36 +0100

    CF: Initial CarbonFibers Bringup for Lux
    Right now its just a proof of the design concept, no usage at all yet.

---
+++ app/src/main/java/org/carbonrom/carbonfibers/MainActivity.java
@@ -0,0 +1,96 @@
+package org.carbonrom.carbonfibers;
+
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        initDrawer(R.layout.activity_main, true);
+    }
+
+    private void initDrawer(int initLayout, boolean openDrawer){
+        setContentView(initLayout);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        if(openDrawer) {
+            drawer.openDrawer(GravityCompat.START);
+        }else{
+            drawer.closeDrawer(GravityCompat.START);
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        Fragment fragment = null;
+
+        Class fragmentClass = null;
+
+        if (id == R.id.nav_camera) {
+            initDrawer(R.layout.test,true);
+            // Example of calling a fragment
+            fragmentClass = MyFragment.class;
+        } else if (id == R.id.nav_gallery) {
+            initDrawer(R.layout.test,true);
+        } else if (id == R.id.nav_slideshow) {
+            initDrawer(R.layout.test,true);
+        } else if (id == R.id.nav_manage) {
+            initDrawer(R.layout.test,true);
+        } else if (id == R.id.nav_share) {
+            initDrawer(R.layout.activity_main,true);
+        } else if (id == R.id.nav_send) {
+            initDrawer(R.layout.activity_main,true);
+        }
+
+        try {
+            if (fragmentClass != null) {
+                fragment = (Fragment) fragmentClass.newInstance();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        if(fragment != null) {
+            // Insert the fragment by replacing any existing fragment
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            fragmentManager.beginTransaction().replace(R.id.flContent, fragment).commit();
+        }
+
+        return true;
+    }
+}
