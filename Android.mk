74d584d764db021c1f7eccad6fb4f04c6d0e47cd
Christian Oder <myself5@carbonrom.org>
2016-01-15 14:23:13 +0100

    CF: Fix Makefile to include proper libs
    Change-Id: I602d8c7262eb4f7df808e08f16cbf44b7fdf8c70

--- Android.mk
+++ Android.mk
@@ -1,16 +1,31 @@
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
+LOCAL_STATIC_JAVA_LIBRARIES := android-support-v4 android-support-design android-support-v7-appcompat
+
 LOCAL_MODULE_TAGS := optional
 
-# Only compile source java files in this apk.
-LOCAL_SRC_FILES := $(call all-java-files-under, src)
+LOCAL_SRC_FILES := \
+        $(call all-java-files-under, src)
+
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res \
+		      $(LOCAL_PATH)/../../../$(SUPPORT_LIBRARY_ROOT)/v7/appcompat/res \
+		      $(LOCAL_PATH)/../../../$(SUPPORT_LIBRARY_ROOT)/design/res
 
 LOCAL_PACKAGE_NAME := CarbonFibers
+LOCAL_CERTIFICATE := platform
+LOCAL_PRIVILEGED_MODULE := true
 
-LOCAL_SDK_VERSION := current
+LOCAL_AAPT_FLAGS := \
+    --auto-add-overlay \
+    --extra-packages android.support.design \
+    --extra-packages android.support.v7.appcompat
+
+# include frameworks/base/packages/SettingsLib/common.mk
 
 include $(BUILD_PACKAGE)
 
 # Use the following include to make our test apk.
+ifeq (,$(ONE_SHOT_MAKEFILE))
 include $(call all-makefiles-under,$(LOCAL_PATH))
+endif
