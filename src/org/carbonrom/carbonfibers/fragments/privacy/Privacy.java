df59be11aa251116a3d1b4216dd7d40fa743acca
Christian Oder <myself5@carbonrom.org>
2016-09-12 14:15:04 +0200

    CarbonFibers: Initial cr-8.0 bringup
    The Code as well as the Settings implementation is based off DUTweaks
    from the Dirty Unicorns Project.
    
    Huge thanks to them!
    
    Change-Id: I3d594158c474742af2cf2572307ce5120d7a1aea
    Signed-off-by: Joe Maples <joe@frap129.org>
    
    Fibers: fix main menu settings crash
    
    Change-Id: Ide122956fe593c0a55a44380acbff778d37d85da
    Signed-off-by: gtpitch <bmontagu@gmail.com>
    Signed-off-by: Joe Maples <joe@frap129.org>
    
    CF: restructure and add Privacy category
    
    Change-Id: Iec6b39538d42aba7d19449e2e09af7a2f6ce9813
    Signed-off-by: Joe Maples <joe@frap129.org>

---
+++ src/org/carbonrom/carbonfibers/fragments/privacy/Privacy.java
@@ -0,0 +1,98 @@
+/*
+ * Copyright (C) 2016 CarbonROM
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.carbonrom.carbonfibers.fragments.privacy;
+
+import android.content.Context;
+import android.content.ContentResolver;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.os.UserHandle;
+import android.preference.ListPreference;
+import android.preference.SwitchPreference;
+import android.preference.Preference;
+import android.preference.PreferenceCategory;
+import android.preference.PreferenceScreen;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.provider.Settings;
+import android.provider.SearchIndexableResource;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+import com.android.settings.Utils;
+import com.android.settings.search.BaseSearchIndexProvider;
+import com.android.settings.search.Indexable;
+import com.android.settingslib.search.SearchIndexable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@SearchIndexable
+public class Privacy extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener, Indexable {
+    private static final String TAG = "Privacy";
+    private static final String CARBON_STATS = "crstats";
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.privacy);
+
+        ContentResolver resolver = getActivity().getContentResolver();
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsEvent.CARBONFIBERS;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+        final String key = preference.getKey();
+        return true;
+    }
+
+    public static final Indexable.SearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
+            new BaseSearchIndexProvider() {
+                @Override
+                public List<SearchIndexableResource> getXmlResourcesToIndex(Context context,
+                                                                            boolean enabled) {
+                    final ArrayList<SearchIndexableResource> result = new ArrayList<>();
+                    final SearchIndexableResource sir = new SearchIndexableResource(context);
+                    sir.xmlResId = R.xml.privacy;
+                    result.add(sir);
+                    return result;
+                }
+
+                @Override
+                public List<String> getNonIndexableKeys(Context context) {
+                    ArrayList<String> keys = super.getNonIndexableKeys(context);
+                    keys.add(CARBON_STATS);
+                    return keys;
+                }
+            };
+}
