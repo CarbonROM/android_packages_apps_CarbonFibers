593ef9ac61469e1d4c06f5026bcdd58386d59359
XXMrHyde <xxmrhyde@gmx.co.uk>
2017-01-21 15:24:07 -0800

    Add customizable portrait/landscape QS rows [2/2]
    Change-Id: I38a02ee62a331242d279b8ee9a78368eee9af088
    Signed-off-by: Adin Kwok <adin.kwok@carbonrom.org>

--- src/org/carbonrom/carbonfibers/fragments/statusbar/QuickSettings.java
+++ src/org/carbonrom/carbonfibers/fragments/statusbar/QuickSettings.java
@@ -47,11 +47,15 @@ public class QuickSettings extends SettingsPreferenceFragment implements
     private static final String PREF_LOCK_QS_DISABLED = "lockscreen_qs_disabled";
     private static final String PREF_QS_DATA_ADVANCED = "qs_data_advanced";
     private static final String PREF_COLUMNS = "qs_layout_columns";
+    private static final String PREF_ROWS_PORTRAIT = "qs_rows_portrait";
+    private static final String PREF_ROWS_LANDSCAPE = "qs_rows_landscape";
 
     private SwitchPreference mLockQsDisabled;
     private SwitchPreference mQsDataAdvanced;
 
     private CustomSeekBarPreference mQsColumns;
+    private CustomSeekBarPreference mRowsPortrait;
+    private CustomSeekBarPreference mRowsLandscape;
 
     private static final int MY_USER_ID = UserHandle.myUserId();
 
@@ -88,6 +92,19 @@ public class QuickSettings extends SettingsPreferenceFragment implements
                 Settings.System.QS_LAYOUT_COLUMNS, 3);
         mQsColumns.setValue(columnsQs / 1);
         mQsColumns.setOnPreferenceChangeListener(this);
+
+        mRowsPortrait = (CustomSeekBarPreference) findPreference(PREF_ROWS_PORTRAIT);
+        int rowsPortrait = Settings.System.getInt(resolver,
+                Settings.System.QS_ROWS_PORTRAIT, 3);
+        mRowsPortrait.setValue(rowsPortrait / 1);
+        mRowsPortrait.setOnPreferenceChangeListener(this);
+
+        int defaultValue = getResources().getInteger(com.android.internal.R.integer.config_qs_num_rows_landscape_default);
+        mRowsLandscape = (CustomSeekBarPreference) findPreference(PREF_ROWS_LANDSCAPE);
+        int rowsLandscape = Settings.System.getInt(resolver,
+                Settings.System.QS_ROWS_LANDSCAPE, defaultValue);
+        mRowsLandscape.setValue(rowsLandscape / 1);
+        mRowsLandscape.setOnPreferenceChangeListener(this);
     }
 
     @Override
@@ -122,6 +139,16 @@ public class QuickSettings extends SettingsPreferenceFragment implements
             int qsColumns = (Integer) objValue;
             Settings.System.putInt(getActivity().getContentResolver(),
                     Settings.System.QS_LAYOUT_COLUMNS, qsColumns * 1);
+            return true;
+        } else if (preference == mRowsPortrait) {
+            int rowsPortrait = (Integer) objValue;
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.QS_ROWS_PORTRAIT, rowsPortrait * 1);
+            return true;
+        } else if (preference == mRowsLandscape) {
+            int rowsLandscape = (Integer) objValue;
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.QS_ROWS_LANDSCAPE, rowsLandscape * 1);
             return true;
         }
         return false;
