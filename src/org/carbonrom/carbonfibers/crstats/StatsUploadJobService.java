45f005817b77c111a1cf3635e6e37e349ffcaa8f
Michael W <baddaemon87@gmail.com>
2017-02-05 00:25:32 +0100

    CarbonFibers: Clean out imports
    * Remove unused imports
    
    Change-Id: I347d7e3397ea4960e182885e7c1a7c60d6d99213

--- src/org/carbonrom/carbonfibers/crstats/StatsUploadJobService.java
+++ src/org/carbonrom/carbonfibers/crstats/StatsUploadJobService.java
@@ -29,12 +29,10 @@ import org.json.JSONObject;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
-import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Collections;
