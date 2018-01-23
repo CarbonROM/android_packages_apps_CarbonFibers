3711083a9a5e895413a699c187ba8ac2c85dcc4d
Joe Maples <joe@frap129.org>
2018-01-22 21:19:34 -0500

    CF: Smart Pixels [2/2]
    Change-Id: I55775e02e849ad5db5b1187be0ca77b7c09f1081
    Signed-off-by: Joe Maples <joe@frap129.org>
    Signed-off-by: Adin Kwok <adin.kwok@carbonrom.org>

---
+++ src/org/carbonrom/carbonfibers/fragments/system/SmartPixels.java
@@ -0,0 +1,96 @@
+/*
+ * Copyright (C) 2018 CarbonROM
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.carbonrom.carbonfibers.fragments.system;
+
+import android.content.Context;
+import android.content.ContentResolver;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.os.UserHandle;
+import android.preference.ListPreference;
+import android.preference.SwitchPreference;
+import android.preference.Preference;
+import android.preference.PreferenceCategory;
+import android.preference.PreferenceManager;
+import android.preference.PreferenceScreen;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.provider.Settings;
+
+import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.Utils
+import com.android.settings.carbon.SystemSettingSwitchPreference;
+
+public class SmartPixels extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+    private static final String TAG = "SmartPixels";
+
+    private static final String ENABLE = "smart_pixels_enable";
+
+    private SystemSettingSwitchPreference mSmartPixelsEnable;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.smart_pixels);
+
+        ContentResolver resolver = getActivity().getContentResolver();
+
+        mSmartPixelsEnable = (SystemSettingSwitchPreference) findPreference(ENABLE);
+
+        updateDependency();
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsEvent.CARBONFIBERS;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+        final String key = preference.getKey();
+        updateDependency();
+        return true;
+    }
+
+    private void updateDependency() {
+        int mSmartPixelsOnPowerSave = Settings.System.getIntForUser(
+                resolver, Settings.System.SMART_PIXELS_ON_POWER_SAVE,
+                0, mCurrentUserId);
+        int mSmartPixelsEnable = Settings.System.getIntForUser(
+                resolver, Settings.System.SMART_PIXELS_ENABLE,
+                0, mCurrentUserId);
+        final PowerManager pm = (PowerManager) mContext.getSystemService(Context.POWER_SERVICE);
+        if ((mSmartPixelsOnPowerSave != 0) && pm.isPowerSaveMode()) {
+            mSmartPixelsEnable.setEnabled(false);
+        } else {
+            mSmartPixelsEnable.setEnabled(true);
+        }
+    }
+
+}
