76140df004b96597fbd9054d6a177bc6f64e6d4f
Adin Kwok <adin.kwok@carbonrom.org>
2018-03-08 10:24:54 +0000

    CF: Remove Misc menu and improve stats menu
    Added proper warning dialog for stats
    Moved screen off animations to System
    More "misc" (hahaha) improvements
    
    Change-Id: Ie5a8fa87a80da9ab378c8bdf59fcf43844263c62

--- src/org/carbonrom/carbonfibers/crstats/AnonymousStats.java
+++ src/org/carbonrom/carbonfibers/crstats/AnonymousStats.java
@@ -16,9 +16,16 @@
 
 package org.carbonrom.carbonfibers.crstats;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.android.internal.logging.MetricsLogger;
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
@@ -42,11 +49,39 @@ public class AnonymousStats extends SettingsPreferenceFragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.anonymous_stats);
+        setHasOptionsMenu(true);
     }
 
     @Override
     public int getMetricsCategory() {
         return MetricsEvent.CARBONFIBERS;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        menu.add(0, MENU_HELP, 0, R.string.anonymous_statistics_warning_title)
+                .setIcon(R.drawable.ic_carbonfibers_info)
+                .setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case MENU_HELP:
+                showDialogInner(MENU_HELP);
+                Toast.makeText(getActivity(),
+                (R.string.carbonfibers_dialog_toast),
+                Toast.LENGTH_LONG).show();
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    private void showDialogInner(int id) {
+        DialogFragment newFragment = MyAlertDialogFragment.newInstance(id);
+        newFragment.setTargetFragment(this, 0);
+        newFragment.show(getFragmentManager(), "dialog " + id);
     }
 
     public static void updateLastSynced(Context context) {
@@ -77,4 +112,38 @@ public class AnonymousStats extends SettingsPreferenceFragment {
         setLastJobId(context, lastId);
         return lastId;
     }
+
+    public static class MyAlertDialogFragment extends DialogFragment {
+
+        public static MyAlertDialogFragment newInstance(int id) {
+            MyAlertDialogFragment frag = new MyAlertDialogFragment();
+            Bundle args = new Bundle();
+            args.putInt("id", id);
+            frag.setArguments(args);
+            return frag;
+        }
+
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            int id = getArguments().getInt("id");
+            switch (id) {
+                case MENU_HELP:
+                    return new AlertDialog.Builder(getActivity())
+                    .setTitle(R.string.anonymous_statistics_warning_title)
+                    .setMessage(R.string.anonymous_statistics_warning)
+                    .setCancelable(false)
+                    .setNegativeButton(R.string.dlg_ok,
+                        new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                        }
+                    })
+                    .create();
+            }
+            throw new IllegalArgumentException("unknown id " + id);
+        }
+
+        @Override
+        public void onCancel(DialogInterface dialog) {
+        }
+    }
+
 }
