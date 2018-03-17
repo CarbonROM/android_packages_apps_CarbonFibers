7627b2e35ce7a7583d81da654ab5eae03ac7735c
Joe Maples <joe@frap129.org>
2018-03-17 19:38:19 -0400

    Fibers: add toggle to allow untrusted overlays (2/2)
    Change-Id: I79623c892ac61cb884b1ffde7a023ed1c4b45990
    Signed-off-by: Brett Montague <bmontagu@gmail.com>

--- src/org/carbonrom/carbonfibers/fragments/privacy/Privacy.java
+++ src/org/carbonrom/carbonfibers/fragments/privacy/Privacy.java
@@ -16,10 +16,17 @@
 
 package org.carbonrom.carbonfibers.fragments.privacy;
 
+import android.app.AlertDialog;
+import android.app.AlertDialog.Builder;
 import android.content.Context;
 import android.content.ContentResolver;
+import android.content.DialogInterface;
+import static android.content.DialogInterface.BUTTON_NEGATIVE;
+import static android.content.DialogInterface.BUTTON_POSITIVE;
+import android.content.DialogInterface.OnClickListener;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.os.PowerManager;
 import android.os.UserHandle;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.Preference.OnPreferenceChangeListener;
@@ -36,8 +43,10 @@ import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
 import com.android.settings.Utils;
 
 public class Privacy extends SettingsPreferenceFragment implements
-        Preference.OnPreferenceChangeListener {
+        Preference.OnPreferenceChangeListener, DialogInterface.OnClickListener {
     private static final String TAG = "Privacy";
+    private SwitchPreference mUntrustedOverlay;
+    private boolean mIsTrustedOverlayValue;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -46,6 +55,12 @@ public class Privacy extends SettingsPreferenceFragment implements
         addPreferencesFromResource(R.xml.privacy);
 
         ContentResolver resolver = getActivity().getContentResolver();
+
+        mUntrustedOverlay = (SwitchPreference) findPreference("untrusted_overlay_toggle");
+        mIsTrustedOverlayValue = Settings.Secure.getIntForUser(getContentResolver(),
+                Settings.Secure.UNTRUSTED_OVERLAY_TOGGLE, 0, UserHandle.USER_CURRENT) == 1;
+        mUntrustedOverlay.setOnPreferenceChangeListener(this);
+        mUntrustedOverlay.setChecked(mIsTrustedOverlayValue);
     }
 
     @Override
@@ -63,8 +78,41 @@ public class Privacy extends SettingsPreferenceFragment implements
         super.onPause();
     }
 
+    private void alertReboot() {
+        new AlertDialog.Builder(getActivity())
+                .setTitle(R.string.untrusted_overlay_alert_title)
+                .setMessage(R.string.untrusted_overlay_alert_message)
+                .setPositiveButton(R.string.reboot, this)
+                .setNegativeButton(android.R.string.cancel, this)
+                .setCancelable(false)
+                .show();
+    }
+
+    @Override
+    public void onClick(DialogInterface dialog, int which) {
+        switch (which) {
+            case BUTTON_NEGATIVE:
+                mUntrustedOverlay.setChecked(mIsTrustedOverlayValue);
+                dialog.dismiss();
+                break;
+            case BUTTON_POSITIVE:
+                mIsTrustedOverlayValue = Settings.Secure.getIntForUser(getContentResolver(),
+                        Settings.Secure.UNTRUSTED_OVERLAY_TOGGLE, 0, UserHandle.USER_CURRENT) == 1;
+                Settings.Secure.putIntForUser(getContentResolver(),
+                        Settings.Secure.UNTRUSTED_OVERLAY_TOGGLE,
+                        mIsTrustedOverlayValue ? 0 : 1, UserHandle.USER_CURRENT);
+                PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
+                pm.reboot(null);
+                dialog.dismiss();
+                break;
+            default:
+                break;
+        }
+    }
+
     public boolean onPreferenceChange(Preference preference, Object objValue) {
-        final String key = preference.getKey();
+        if (preference.equals(mUntrustedOverlay))
+            alertReboot();
         return true;
     }
 
