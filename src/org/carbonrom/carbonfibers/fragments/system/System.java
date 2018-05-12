43489eec24202dbddcd713818c8b676332f200eb
maxwen <max.weninger@gmail.com>
2018-05-12 19:00:08 +0200

    CF: make aspect ratio hack configable on app level
    Change-Id: I556a7ef2ea8bcb7d1a4ad9607e4c0bc3e76aa8d3

--- src/org/carbonrom/carbonfibers/fragments/system/System.java
+++ src/org/carbonrom/carbonfibers/fragments/system/System.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016 CarbonROM
+ * Copyright (C) 2016-2018 CarbonROM
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -42,11 +42,17 @@ public class System extends SettingsPreferenceFragment implements
     private static final String IMMERSIVE_RECENTS = "immersive_recents";
     private static final String RECENTS_CLEAR_ALL_LOCATION = "recents_clear_all_location";
     private static final String SCREEN_OFF_ANIMATION = "screen_off_animation";
+    private static final String SMART_PIXELS = "smart_pixels";
+    private static final String FORCE_ASPECT_RATIO = "force_aspect_ratio";
+    private static final String FORCE_ASPECT_RATIO_ENABLED = "aspect_ratio_apps_enabled";
 
     private ListPreference mRecentsClearAllLocation;
     private ListPreference mImmersiveRecents;
     private ListPreference mScreenOffAnimation;
     private ContentResolver resolver;
+    private SwitchPreference mAspectRatioSwitch;
+    private PreferenceScreen mForceAspectRatioApps;
+    private boolean mAspectRatioEnabled;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -80,10 +86,26 @@ public class System extends SettingsPreferenceFragment implements
 
         boolean enableSmartPixels = getContext().getResources().
                 getBoolean(com.android.internal.R.bool.config_enableSmartPixels);
-        Preference SmartPixels = findPreference("smart_pixels");
+        Preference SmartPixels = findPreference(SMART_PIXELS);
 
         if (!enableSmartPixels){
             prefSet.removePreference(SmartPixels);
+        }
+
+        mAspectRatioSwitch = (SwitchPreference) findPreference(FORCE_ASPECT_RATIO_ENABLED);
+        mAspectRatioEnabled = Settings.System.getIntForUser(resolver,
+                Settings.System.ASPECT_RATIO_APPS_ENABLED, 0, UserHandle.USER_CURRENT) == 1;
+        mAspectRatioSwitch.setOnPreferenceChangeListener(this);
+        mAspectRatioSwitch.setChecked(mAspectRatioEnabled);
+
+        boolean enableForceAspectRatio = getContext().getResources().
+                getBoolean(com.android.internal.R.bool.config_haveHigherAspectRatioScreen);
+        mForceAspectRatioApps = (PreferenceScreen) findPreference(FORCE_ASPECT_RATIO);
+        mForceAspectRatioApps.setEnabled(mAspectRatioEnabled);
+
+        if (!enableForceAspectRatio){
+            prefSet.removePreference(mAspectRatioSwitch);
+            prefSet.removePreference(mForceAspectRatioApps);
         }
     }
 
@@ -104,24 +126,29 @@ public class System extends SettingsPreferenceFragment implements
 
     public boolean onPreferenceChange(Preference preference, Object objValue) {
         final String key = preference.getKey();
-        if (preference == mImmersiveRecents) {
+        if (preference.equals(mImmersiveRecents)) {
             Settings.System.putInt(resolver, Settings.System.IMMERSIVE_RECENTS,
                     Integer.valueOf((String) objValue));
             mImmersiveRecents.setValue(String.valueOf(objValue));
             mImmersiveRecents.setSummary(mImmersiveRecents.getEntry());
             return true;
-        } else if (preference == mRecentsClearAllLocation) {
+        } else if (preference.equals(mRecentsClearAllLocation)) {
             int location = Integer.valueOf((String) objValue);
             int index = mRecentsClearAllLocation.findIndexOfValue((String) objValue);
             Settings.System.putIntForUser(getContentResolver(),
                     Settings.System.RECENTS_CLEAR_ALL_LOCATION, location, UserHandle.USER_CURRENT);
             mRecentsClearAllLocation.setSummary(mRecentsClearAllLocation.getEntries()[index]);
             return true;
-        } else if (preference == mScreenOffAnimation) {
+        } else if (preference.equals(mScreenOffAnimation)) {
             Settings.System.putInt(resolver,
                     Settings.System.SCREEN_OFF_ANIMATION, Integer.valueOf((String) objValue));
             int valueIndex = mScreenOffAnimation.findIndexOfValue((String) objValue);
             mScreenOffAnimation.setSummary(mScreenOffAnimation.getEntries()[valueIndex]);
+            return true;
+        } else if (preference.equals(mAspectRatioSwitch)) {
+            Settings.System.putIntForUser(resolver,
+                    Settings.System.ASPECT_RATIO_APPS_ENABLED, (boolean)objValue ? 0 : 1, UserHandle.USER_CURRENT);
+            mForceAspectRatioApps.setEnabled((boolean)objValue);
             return true;
         }
         return false;
