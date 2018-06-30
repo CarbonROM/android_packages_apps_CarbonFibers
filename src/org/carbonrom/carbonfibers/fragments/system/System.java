7b1797684d33f8456b75b003f790251f6821192d
maxwen <max.weninger@gmail.com>
2018-06-30 14:07:37 +0530

    Fibers: Make aspect ratio hack configable on app level [2/2]
    Change-Id: I95527d2c93444df5c1281f47c2ca6e82d21d8844

--- src/org/carbonrom/carbonfibers/fragments/system/System.java
+++ src/org/carbonrom/carbonfibers/fragments/system/System.java
@@ -23,6 +23,16 @@ import android.os.Bundle;
 import android.os.UserHandle;
 import android.provider.Settings;
 import android.provider.SearchIndexableResource;
+import android.text.TextUtils;
+import android.app.Activity;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.ContentResolver;
+import android.content.DialogInterface;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.os.UserHandle;
+import android.provider.Settings;
 
 import androidx.preference.Preference;
 import androidx.preference.PreferenceCategory;
@@ -42,20 +52,59 @@ import com.android.settingslib.search.Indexable;
 import com.android.settingslib.search.SearchIndexable;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
 
+import org.carbonrom.carbonfibers.preference.AppMultiSelectListPreference;
+import org.carbonrom.carbonfibers.preference.ScrollAppsViewPreference;
+
 @SearchIndexable
-public class System extends SettingsPreferenceFragment implements Indexable {
+public class System extends SettingsPreferenceFragment implements Preference.OnPreferenceChangeListener, Indexable {
     private static final String TAG = "System";
     private static final String AGGRESSIVE_BATTERY ="aggressive_battery";
     private static final String SMART_PIXELS = "smart_pixels";
     private static final String KEY_SYSTEM_POWER_SAVE_PREFERENCE_CATEGORY = "system_power_save_preference_category";
+    private static final String KEY_ASPECT_RATIO_APPS_ENABLED = "aspect_ratio_apps_enabled";
+    private static final String KEY_ASPECT_RATIO_APPS_LIST = "aspect_ratio_apps_list";
+    private static final String KEY_ASPECT_RATIO_CATEGORY = "aspect_ratio_category";
+    private static final String KEY_ASPECT_RATIO_APPS_LIST_SCROLLER = "aspect_ratio_apps_list_scroller";
+
+    private AppMultiSelectListPreference mAspectRatioAppsSelect;
+    private ScrollAppsViewPreference mAspectRatioApps;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ContentResolver resolver = getActivity().getContentResolver();
 
         addPreferencesFromResource(R.xml.system);
+
+        final PreferenceCategory aspectRatioCategory =
+                (PreferenceCategory) getPreferenceScreen().findPreference(KEY_ASPECT_RATIO_CATEGORY);
+        final boolean supportMaxAspectRatio =
+                getResources().getBoolean(com.android.internal.R.bool.config_haveHigherAspectRatioScreen);
+        if (!supportMaxAspectRatio) {
+                getPreferenceScreen().removePreference(aspectRatioCategory);
+        } else {
+        mAspectRatioAppsSelect =
+                (AppMultiSelectListPreference) findPreference(KEY_ASPECT_RATIO_APPS_LIST);
+        mAspectRatioApps =
+                (ScrollAppsViewPreference) findPreference(KEY_ASPECT_RATIO_APPS_LIST_SCROLLER);
+        final String valuesString = Settings.System.getString(getContentResolver(),
+                Settings.System.ASPECT_RATIO_APPS_LIST);
+        List<String> valuesList = new ArrayList<String>();
+        if (!TextUtils.isEmpty(valuesString)) {
+            valuesList.addAll(Arrays.asList(valuesString.split(":")));
+            mAspectRatioApps.setVisible(true);
+            mAspectRatioApps.setValues(valuesList);
+        } else {
+            mAspectRatioApps.setVisible(false);
+        }
+        mAspectRatioAppsSelect.setValues(valuesList);
+        mAspectRatioAppsSelect.setOnPreferenceChangeListener(this);
+        }
 
         final PreferenceCategory powerSaveCategory =
                 (PreferenceCategory) getPreferenceScreen().findPreference(KEY_SYSTEM_POWER_SAVE_PREFERENCE_CATEGORY);
@@ -71,6 +120,26 @@ public class System extends SettingsPreferenceFragment implements Indexable {
     @Override
     public int getMetricsCategory() {
         return MetricsEvent.CARBONFIBERS;
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        ContentResolver resolver = getActivity().getContentResolver();
+        if (preference == mAspectRatioAppsSelect) {
+            Collection<String> valueList = (Collection<String>) newValue;
+            mAspectRatioApps.setVisible(false);
+            if (valueList != null) {
+                Settings.System.putString(getContentResolver(),
+                        Settings.System.ASPECT_RATIO_APPS_LIST, TextUtils.join(":", valueList));
+                mAspectRatioApps.setVisible(true);
+                mAspectRatioApps.setValues(valueList);
+            } else {
+                Settings.System.putString(getContentResolver(),
+                Settings.System.ASPECT_RATIO_APPS_LIST, "");
+            }
+            return true;
+        }
+        return false;
     }
 
     public static final SearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
