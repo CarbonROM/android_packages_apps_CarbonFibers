b6a01c8d5be8a98ab90a4bfa4044db3d0618848f
Adin Kwok <adin.kwok@carbonrom.org>
2018-08-11 17:21:11 -0700

    CF: Move gestures to CarbonFibers [2/2]
    Change-Id: Iccb884c7b190fbae3837e63179c4446c6c45cc53

--- src/org/carbonrom/carbonfibers/CarbonFibers.java
+++ src/org/carbonrom/carbonfibers/CarbonFibers.java
@@ -63,6 +63,7 @@ import java.util.List;
 public class CarbonFibers extends SettingsPreferenceFragment implements Indexable {
 
     private static final int MENU_HELP  = 0;
+    private static final String KEY_GESTURE_MAIN_CATEGORY = "gestures_main_category";
     private static final String KEY_SYSTEM_MAIN_CATEGORY = "system_main_category";
     private static final String KEY_BUTTONS_MAIN_CATEGORY = "buttons_main_category";
     private static final String KEY_STATUSBAR_MAIN_CATEGORY = "statusbar_main_category";
@@ -149,6 +150,7 @@ public class CarbonFibers extends SettingsPreferenceFragment implements Indexabl
                 @Override
                 public List<String> getNonIndexableKeys(Context context) {
                     ArrayList<String> keys = new ArrayList<String>();
+                    keys.add(KEY_GESTURE_MAIN_CATEGORY);
                     keys.add(KEY_BUTTONS_MAIN_CATEGORY);
                     keys.add(KEY_STATUSBAR_MAIN_CATEGORY);
                     keys.add(KEY_SYSTEM_MAIN_CATEGORY);
