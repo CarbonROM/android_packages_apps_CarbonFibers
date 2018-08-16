802fec6d2fdb493f1048f532b91535407ad9fe4a
Adin Kwok <adin.kwok@carbonrom.org>
2018-08-15 17:40:24 -0700

    Switch to CustomSettingsPreferenceFragment
    Change-Id: Ia1f62a5225f0ef69893d1564a5abcd76b4d32b09

--- src/org/carbonrom/carbonfibers/fragments/privacy/Privacy.java
+++ src/org/carbonrom/carbonfibers/fragments/privacy/Privacy.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016 CarbonROM
+ * Copyright (C) 2018 CarbonROM
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,23 +17,14 @@
 package org.carbonrom.carbonfibers.fragments.privacy;
 
 import android.content.Context;
-import android.content.ContentResolver;
-import android.content.res.Resources;
 import android.os.Bundle;
-import android.os.UserHandle;
-import android.preference.ListPreference;
-import android.preference.SwitchPreference;
-import android.preference.Preference;
-import android.preference.PreferenceCategory;
-import android.preference.PreferenceScreen;
-import android.preference.Preference.OnPreferenceChangeListener;
-import android.provider.Settings;
 import android.provider.SearchIndexableResource;
 
+import androidx.preference.Preference;
+import androidx.preference.SwitchPreference;
+
 import com.android.settings.R;
-import com.android.settings.SettingsPreferenceFragment;
-import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
-import com.android.settings.Utils;
+import com.android.settings.carbon.CustomSettingsPreferenceFragment;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settings.search.Indexable;
 import com.android.settingslib.search.SearchIndexable;
@@ -42,8 +33,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @SearchIndexable
-public class Privacy extends SettingsPreferenceFragment implements
-        Preference.OnPreferenceChangeListener, Indexable {
+public class Privacy extends CustomSettingsPreferenceFragment implements Indexable {
     private static final String TAG = "Privacy";
     private static final String CARBON_STATS = "crstats";
 
@@ -52,28 +42,6 @@ public class Privacy extends SettingsPreferenceFragment implements
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.privacy);
-
-        ContentResolver resolver = getActivity().getContentResolver();
-    }
-
-    @Override
-    public int getMetricsCategory() {
-        return MetricsEvent.CARBONFIBERS;
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-    }
-
-    public boolean onPreferenceChange(Preference preference, Object objValue) {
-        final String key = preference.getKey();
-        return true;
     }
 
     public static final SearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
