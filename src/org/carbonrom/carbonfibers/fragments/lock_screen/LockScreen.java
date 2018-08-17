12d0ccd4579b45bb6a3ba28df6f3e1d3a649747d
Adin Kwok <adin.kwok@carbonrom.org>
2018-08-16 18:49:17 -0700

    CF: Add lock screen fragment
    Change-Id: Ie0f0803abb4ee61ab548dc531c29cdafa6cd261a

---
+++ src/org/carbonrom/carbonfibers/fragments/lock_screen/LockScreen.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (C) 2018 CarbonROM
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.carbonrom.carbonfibers.fragments.lock_screen;
+
+import android.os.Bundle;
+import android.provider.SearchIndexableResource;
+
+import com.android.settings.R;
+import com.android.settings.carbon.CustomSettingsPreferenceFragment;
+import com.android.settings.search.Indexable;
+
+import com.android.settingslib.search.SearchIndexable;
+
+@SearchIndexable
+public class LockScreen extends CustomSettingsPreferenceFragment implements Indexable {
+    private static final String TAG = "LockScreen";
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.lock_screen);
+    }
+
+    public static final Indexable.SearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
+            new BaseSearchIndexProvider() {
+                @Override
+                public List<SearchIndexableResource> getXmlResourcesToIndex(Context context,
+                                                                            boolean enabled) {
+                    ArrayList<SearchIndexableResource> result =
+                            new ArrayList<SearchIndexableResource>();
+
+                    SearchIndexableResource sir = new SearchIndexableResource(context);
+                    sir.xmlResId = R.xml.lock_screen;
+                    result.add(sir);
+                    return result;
+                }
+
+                @Override
+                public List<String> getNonIndexableKeys(Context context) {
+                    ArrayList<String> result = new ArrayList<String>();
+                    return result;
+                }
+            };
+}
