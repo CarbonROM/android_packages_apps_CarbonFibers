a2ed7481912fa96ebf9a361d0909b9aab9d8f272
Adin Kwok <adin.kwok@carbonrom.org>
2018-08-16 18:49:17 -0700

    CF: Add lock screen fragment
    Change-Id: Ie0f0803abb4ee61ab548dc531c29cdafa6cd261a

--- src/org/carbonrom/carbonfibers/CarbonFibers.java
+++ src/org/carbonrom/carbonfibers/CarbonFibers.java
@@ -65,6 +65,7 @@ public class CarbonFibers extends SettingsPreferenceFragment implements Indexabl
     private static final int MENU_HELP  = 0;
     private static final String KEY_GESTURE_MAIN_CATEGORY = "gestures_main_category";
     private static final String KEY_SYSTEM_MAIN_CATEGORY = "system_main_category";
+    private static final String KEY_LOCKSCREEN_MAIN_CATEGORY = "lockscreen_main_category";
     private static final String KEY_MISC_MAIN_CATEGORY = "misc_main_category";
     private static final String KEY_PRIVACY_MAIN_CATEGORY = "privacy_main_category";
 
@@ -165,6 +166,7 @@ public class CarbonFibers extends SettingsPreferenceFragment implements Indexabl
                     ArrayList<String> keys = new ArrayList<String>();
                     keys.add(KEY_GESTURE_MAIN_CATEGORY);
                     keys.add(KEY_SYSTEM_MAIN_CATEGORY);
+                    keys.add(KEY_LOCKSCREEN_MAIN_CATEGORY);
                     keys.add(KEY_MISC_MAIN_CATEGORY);
                     keys.add(KEY_PRIVACY_MAIN_CATEGORY);
                     return keys;
