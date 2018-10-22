05f5952e1a0ebcc78361a372547f49ab2f27375a
maxwen <max.weninger@gmail.com>
2018-10-22 13:50:36 -0700

    CF: Screen off animation config [2/2]
    Change-Id: I786a7c499db557900bfe10d8251955b7e0be9eae
    Signed-off-by: Adin Kwok <adin.kwok@carbonrom.org>

--- src/org/carbonrom/carbonfibers/fragments/system/System.java
+++ src/org/carbonrom/carbonfibers/fragments/system/System.java
@@ -17,17 +17,22 @@
 package org.carbonrom.carbonfibers.fragments.system;
 
 import android.os.Bundle;
+import android.provider.Settings;
+import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceScreen;
 
 import com.android.settings.R;
 import com.android.settings.carbon.CustomSettingsPreferenceFragment;
 
-public class System extends CustomSettingsPreferenceFragment {
+public class System extends CustomSettingsPreferenceFragment
+        implements Preference.OnPreferenceChangeListener {
     private static final String TAG = "System";
     private static final String SMART_PIXELS = "smart_pixels";
     private static final String ADVANCED_REBOOT = "advanced_reboot";
+    private static final String KEY_SCREEN_OFF_ANIMATION = "screen_off_animation";
 
+    private ListPreference mScreenOffAnimation;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -36,6 +41,25 @@ public class System extends CustomSettingsPreferenceFragment {
         addPreferencesFromResource(R.xml.system);
         addCustomPreference(findPreference(ADVANCED_REBOOT), SECURE_TWO_STATE, STATE_ON);
         updateSmartPixelsPreference();
+
+        mScreenOffAnimation = (ListPreference) findPreference(KEY_SCREEN_OFF_ANIMATION);
+        int screenOffAnimation = Settings.Global.getInt(getContentResolver(),
+                Settings.Global.SCREEN_OFF_ANIMATION, 0);
+        mScreenOffAnimation.setValue(Integer.toString(screenOffAnimation));
+        mScreenOffAnimation.setSummary(mScreenOffAnimation.getEntry());
+        mScreenOffAnimation.setOnPreferenceChangeListener(this);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mScreenOffAnimation) {
+            int value = Integer.valueOf((String) newValue);
+            int index = mScreenOffAnimation.findIndexOfValue((String) newValue);
+            mScreenOffAnimation.setSummary(mScreenOffAnimation.getEntries()[index]);
+            Settings.Global.putInt(getContentResolver(), Settings.Global.SCREEN_OFF_ANIMATION, value);
+            return true;
+        }
+        return false;
     }
 
     private void updateSmartPixelsPreference() {
