e8d031eed6df139e2ba10ea2382fc015de269d28
cyanogen <shade@chemlab.org>
2018-10-22 22:21:57 -0700

    CF: Add lock screen visualizer toggle [2/2]
    Change-Id: I71fb6af56de747a35ab66bfafa0a1634424bfa5c

--- src/org/carbonrom/carbonfibers/fragments/lock_screen/LockScreen.java
+++ src/org/carbonrom/carbonfibers/fragments/lock_screen/LockScreen.java
@@ -28,6 +28,7 @@ public class LockScreen extends CustomSettingsPreferenceFragment {
     private static final String LOCKSCREEN_QUICK_UNLOCK_CONTROL = "lockscreen_quick_unlock_control";
     private static final String LOCKSCREEN_PIN_SCRAMBLE_LAYOUT = "lockscreen_scramble_pin_layout";
     private static final String STATUS_BAR_LOCKED_ON_SECURE_KEYGUARD = "status_bar_locked_on_secure_keyguard";
+    private static final String LOCKSCREEN_VISUALIZER = "lockscreen_visualizer";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -38,5 +39,6 @@ public class LockScreen extends CustomSettingsPreferenceFragment {
         addCustomPreference(findPreference(LOCKSCREEN_QUICK_UNLOCK_CONTROL), SYSTEM_TWO_STATE, STATE_OFF);
         addCustomPreference(findPreference(LOCKSCREEN_PIN_SCRAMBLE_LAYOUT), SYSTEM_TWO_STATE, STATE_OFF);
         addCustomPreference(findPreference(STATUS_BAR_LOCKED_ON_SECURE_KEYGUARD), SECURE_TWO_STATE, STATE_OFF);
+        addCustomPreference(findPreference(LOCKSCREEN_VISUALIZER), SYSTEM_TWO_STATE, STATE_OFF);
     }
 }
