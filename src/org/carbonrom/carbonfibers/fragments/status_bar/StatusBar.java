e5e328cecefa2c474dc5876a4a14355a99d039b9
ezio84 <brabus84@gmail.com>
2018-12-14 20:52:50 +0100

    Battery icon style: improve text/hidden switch [2/2]
    if the set style is text or hidden, don't allow
    percentage option (inside/outside icon) to break it
    
    Change-Id: Ibda5cf81c11bc7887cbf850e964681ba800a36b8

--- src/org/carbonrom/carbonfibers/fragments/status_bar/StatusBar.java
+++ src/org/carbonrom/carbonfibers/fragments/status_bar/StatusBar.java
@@ -50,6 +50,8 @@ public class StatusBar extends CustomSettingsPreferenceFragment implements
     private SystemSettingListPreference mStatusBarClock;
     private CustomSeekBarPreference mThreshold;
     private String mCustomCarrierLabelText;
+    private ListPreference mBatteryIconStyle;
+    private ListPreference mBatteryPercentage;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -73,6 +75,24 @@ public class StatusBar extends CustomSettingsPreferenceFragment implements
         mThreshold = (CustomSeekBarPreference) findPreference(NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD);
         mThreshold.setValue(value);
         mThreshold.setOnPreferenceChangeListener(this);
+
+        int batteryStyle = Settings.Secure.getIntForUser(getContentResolver(),
+                Settings.Secure.STATUS_BAR_BATTERY_STYLE, 0/*portrait*/,
+                UserHandle.USER_CURRENT);
+        mBatteryIconStyle = (ListPreference) findPreference("status_bar_battery_style");
+        mBatteryIconStyle.setValue(Integer.toString(batteryStyle));
+        mBatteryIconStyle.setOnPreferenceChangeListener(this);
+
+         int percentage = Settings.System.getIntForUser(getContentResolver(),
+                Settings.System.SHOW_BATTERY_PERCENT, 0,
+                UserHandle.USER_CURRENT);
+        mBatteryPercentage = (ListPreference) findPreference("status_bar_show_battery_percent");
+        mBatteryPercentage.setValue(Integer.toString(percentage));
+        mBatteryPercentage.setOnPreferenceChangeListener(this);
+        boolean hideForcePercentage =
+                batteryStyle == 5 || batteryStyle == 6; /*text or hidden style*/
+        mBatteryPercentage.setEnabled(!hideForcePercentage);
+    
     }
 
     @Override
@@ -122,6 +142,22 @@ public class StatusBar extends CustomSettingsPreferenceFragment implements
                     Settings.System.NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD, val,
                     UserHandle.USER_CURRENT);
              return true;
+        } else if (preference == mBatteryIconStyle) {
+            int value = Integer.valueOf((String) newValue);
+            Settings.Secure.putIntForUser(getContentResolver(),
+                    Settings.Secure.STATUS_BAR_BATTERY_STYLE, value,
+                    UserHandle.USER_CURRENT);
+            boolean hideForcePercentage = value == 5 || value == 6;/*text or hidden style*/
+            mBatteryPercentage.setEnabled(!hideForcePercentage);
+            return true;
+        } else  if (preference == mBatteryPercentage) {
+            int value = Integer.valueOf((String) newValue);
+            Settings.System.putIntForUser(getContentResolver(),
+                    Settings.System.SHOW_BATTERY_PERCENT, value,
+                    UserHandle.USER_CURRENT);
+            boolean hideForcePercentage = value == 5 || value == 6;/*text or hidden style*/
+            mBatteryPercentage.setEnabled(!hideForcePercentage);
+            return true;
         }
         return false;
     }
