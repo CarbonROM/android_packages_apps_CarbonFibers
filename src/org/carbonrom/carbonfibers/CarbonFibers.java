dbe183bf418680f05ba469b4e170a24298489bae
Christian Oder <myself5@carbonrom.org>
2019-04-01 03:55:07 +0200

    CarbonFibers: Cleanup and add Search to Main fragment
    Change-Id: Ied7b10e0667be74fdef4805ea912cdb4b5dd93a2

--- src/org/carbonrom/carbonfibers/CarbonFibers.java
+++ src/org/carbonrom/carbonfibers/CarbonFibers.java
@@ -78,23 +78,8 @@ public class CarbonFibers extends SettingsPreferenceFragment implements Indexabl
     }
 
     @Override
-    public void onActivityCreated(Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-    }
-
-    @Override
-    public void onSaveInstanceState(Bundle saveState) {
-        super.onSaveInstanceState(saveState);
-    }
-
-    @Override
     public int getMetricsCategory() {
         return MetricsEvent.CARBONFIBERS;
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
     }
 
     @Override
@@ -102,6 +87,7 @@ public class CarbonFibers extends SettingsPreferenceFragment implements Indexabl
         menu.add(0, MENU_HELP, 0, R.string.carbonfibers_dialog_title)
                 .setIcon(R.drawable.ic_carbonfibers_info)
                 .setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
+        super.onCreateOptionsMenu(menu, inflater);
     }
 
     @Override
@@ -114,7 +100,7 @@ public class CarbonFibers extends SettingsPreferenceFragment implements Indexabl
                 Toast.LENGTH_LONG).show();
                 return true;
             default:
-                return false;
+                return super.onOptionsItemSelected(item);
         }
     }
 
