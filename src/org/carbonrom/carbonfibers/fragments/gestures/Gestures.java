6f3265a758375745dadc89b759adf8dcb880717e
ezio84 <brabus84@gmail.com>
2019-11-04 21:36:56 +0100

    Double tap to sleep on lockscreen [2/2]
    Change-Id: Ie26a55638b009743a7f791a529a781e75e7e1e45

--- src/org/carbonrom/carbonfibers/fragments/gestures/Gestures.java
+++ src/org/carbonrom/carbonfibers/fragments/gestures/Gestures.java
@@ -48,6 +48,7 @@ public class Gestures extends GestureSettings implements Indexable {
     private static final String KEY_PICK_UP = "gesture_pick_up_input_summary";
     private static final String KEY_PREVENT_RINGING = "gesture_prevent_ringing_summary";
     private static final String KEY_GESTURE_GLOBAL_ACTIONS_PANEL = "gesture_global_actions_panel_summary";
+    private static final String KEY_DOUBLETAP_LOCKSCREEN = "gesture_doubletap_lockscreen_summary";
 
     @Override
     public int getMetricsCategory() {
@@ -92,6 +93,7 @@ public class Gestures extends GestureSettings implements Indexable {
                     keys.add(KEY_PICK_UP);
                     keys.add(KEY_PREVENT_RINGING);
                     keys.add(KEY_GESTURE_GLOBAL_ACTIONS_PANEL);
+                    keys.add(KEY_DOUBLETAP_LOCKSCREEN);
                     return keys;
                 }
             };
