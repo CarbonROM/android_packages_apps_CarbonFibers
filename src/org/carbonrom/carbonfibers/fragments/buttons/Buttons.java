9aed664589f4869af7b4b8ad082ee2971e6c0ead
Christian Oder <myself5@carbonrom.org>
2019-11-10 15:05:30 +0100

    CF: Readd Navbar Layout selection
    Change-Id: Iae6bbef371f09cd2bf71763e5aeef2461f31e35c

--- src/org/carbonrom/carbonfibers/fragments/buttons/Buttons.java
+++ src/org/carbonrom/carbonfibers/fragments/buttons/Buttons.java
@@ -16,12 +16,15 @@
 
 package org.carbonrom.carbonfibers.fragments.buttons;
 
+import android.content.ContentResolver;
 import android.content.Context;
 import android.os.Bundle;
 import android.provider.SearchIndexableResource;
+import android.provider.Settings;
 
 import androidx.preference.Preference;
 import androidx.preference.SwitchPreference;
+import androidx.preference.ListPreference;
 
 import com.android.settings.R;
 import com.android.settings.carbon.CustomSettingsPreferenceFragment;
@@ -33,9 +36,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 @SearchIndexable
-public class Buttons extends CustomSettingsPreferenceFragment implements Indexable {
+public class Buttons extends CustomSettingsPreferenceFragment implements implements Preference.OnPreferenceChangeListener, Indexable {
     private static final String TAG = "Buttons";
+    private static final String NAV_BAR_LAYOUT = "nav_bar_layout";
+    private static final String SYSUI_NAV_BAR = "sysui_nav_bar";
     private static final String INVERSE_NAVBAR = "sysui_nav_bar_inverse";
+
+    private ListPreference mNavBarLayout;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -43,6 +50,22 @@ public class Buttons extends CustomSettingsPreferenceFragment implements Indexab
 
         addPreferencesFromResource(R.xml.buttons);
         addCustomPreference(findPreference(INVERSE_NAVBAR), SECURE_TWO_STATE, STATE_OFF);
+        mNavBarLayout = (ListPreference) findPreference(NAV_BAR_LAYOUT);
+        mNavBarLayout.setOnPreferenceChangeListener(this);
+        String navBarLayoutValue = Settings.Secure.getString(resolver,
+                                        SYSUI_NAV_BAR, "default");
+            mNavBarLayout.setValue(navBarLayoutValue);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+        ContentResolver resolver = getActivity().getContentResolver();
+        if (preference == mNavBarLayout) {
+            Settings.Secure.putString(resolver,
+                        SYSUI_NAV_BAR, (String) objValue);
+            return true;
+        }
+        return false;
     }
 
     public static final SearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
