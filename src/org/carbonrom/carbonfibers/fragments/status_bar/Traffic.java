8e731cc353ccb17329003dc63891262f20631033
Nico60 <ndhouailly@gmail.com>
2020-01-22 17:19:04 +0100

    CF: Hide arrows in Network Traffic indicators [2/2]
    Change-Id: I3b870f0454b81c48290a358a12c3118b8ae037a3

--- src/org/carbonrom/carbonfibers/fragments/status_bar/Traffic.java
+++ src/org/carbonrom/carbonfibers/fragments/status_bar/Traffic.java
@@ -57,6 +57,7 @@ public class Traffic extends SettingsPreferenceFragment implements
 
     private CustomSeekBarPreference mThreshold;
     private SwitchPreference mNetMonitor;
+    private SwitchPreference mNetArrow;
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -72,6 +73,12 @@ public class Traffic extends SettingsPreferenceFragment implements
         mNetMonitor = (SwitchPreference) findPreference("network_traffic_state");
         mNetMonitor.setChecked(isNetMonitorEnabled);
         mNetMonitor.setOnPreferenceChangeListener(this);
+
+        boolean isArrowEnabled = Settings.System.getIntForUser(resolver,
+                Settings.System.NETWORK_TRAFFIC_HIDEARROW, 0, UserHandle.USER_CURRENT) == 1;
+        mNetArrow = (SwitchPreference) findPreference("network_traffic_hidearrow");
+        mNetArrow.setChecked(isArrowEnabled);
+        mNetArrow.setOnPreferenceChangeListener(this);
 
         int value = Settings.System.getIntForUser(resolver,
                 Settings.System.NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD, 1, UserHandle.USER_CURRENT);
@@ -96,6 +103,13 @@ public class Traffic extends SettingsPreferenceFragment implements
             Settings.System.putIntForUser(getContentResolver(),
                     Settings.System.NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD, val,
                     UserHandle.USER_CURRENT);
+            return true;
+        } else if (preference == mNetArrow) {
+            boolean value = (Boolean) objValue;
+            Settings.System.putIntForUser(getActivity().getContentResolver(),
+                    Settings.System.NETWORK_TRAFFIC_HIDEARROW, value ? 1 : 0,
+                    UserHandle.USER_CURRENT);
+            mNetArrow.setChecked(value);
             return true;
         }
         return false;
