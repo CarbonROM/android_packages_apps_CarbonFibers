8701441e2c3eba51b73b1986968f856b9fee896d
Randall Rushing <randall.rushing@gmail.com>
2020-03-23 01:15:49 -0500

    Bring up hardware key support with smartactions
    Change-Id: I5d9c8f3328789495b509bb122274487e4e864428

--- src/org/carbonrom/carbonfibers/fragments/buttons/Buttons.java
+++ src/org/carbonrom/carbonfibers/fragments/buttons/Buttons.java
@@ -35,13 +35,17 @@ import com.android.settings.R;
 import com.android.settings.carbon.CustomSettingsPreferenceFragment;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settings.search.Indexable;
+import com.android.settings.smartactions.ActionFragment;
 import com.android.settingslib.search.SearchIndexable;
+
+import com.android.smartactions.utils.ActionUtils;
+import com.android.smartactions.utils.ActionConstants;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @SearchIndexable
-public class Buttons extends CustomSettingsPreferenceFragment implements Preference.OnPreferenceChangeListener, Indexable {
+public class Buttons extends ActionFragment implements Preference.OnPreferenceChangeListener, Indexable {
     private static final String TAG = "Buttons";
     private static final String CATEGORY_KEYS = "button_keys";
     private static final String NAV_BAR_LAYOUT = "nav_bar_layout";
@@ -49,6 +53,22 @@ public class Buttons extends CustomSettingsPreferenceFragment implements Prefere
     private static final String INVERSE_NAVBAR = "sysui_nav_bar_inverse";
     private static final String VOLUME_BUTTON_MUSIC_CONTROL = "volume_button_music_control";
     private static final String TORCH_POWER_BUTTON_GESTURE = "torch_power_button_gesture";
+    private static final String CATEGORY_HWKEY = "hardware_keys";
+    private static final String CATEGORY_BACK = "back_key";
+    private static final String CATEGORY_HOME = "home_key";
+    private static final String CATEGORY_MENU = "menu_key";
+    private static final String CATEGORY_ASSIST = "assist_key";
+    private static final String CATEGORY_APPSWITCH = "app_switch_key";
+
+    // Masks for checking presence of hardware keys.
+    // Must match values in frameworks/base/core/res/res/values/config.xml
+    public static final int KEY_MASK_HOME = 0x01;
+    public static final int KEY_MASK_BACK = 0x02;
+    public static final int KEY_MASK_MENU = 0x04;
+    public static final int KEY_MASK_ASSIST = 0x08;
+    public static final int KEY_MASK_APP_SWITCH = 0x10;
+    public static final int KEY_MASK_CAMERA = 0x20;
+    public static final int KEY_MASK_VOLUME = 0x40;
 
     private ListPreference mNavBarLayout;
     private ListPreference mTorchPowerButton;
@@ -73,11 +93,11 @@ public class Buttons extends CustomSettingsPreferenceFragment implements Prefere
             mNavBarLayout.setValueIndex(0);
         }
 
-        PreferenceScreen prefSet = getPreferenceScreen();
+        final PreferenceScreen prefScreen = getPreferenceScreen();
         final PreferenceCategory keysCategory =  
-                (PreferenceCategory) prefSet.findPreference(CATEGORY_KEYS);
+                (PreferenceCategory) prefScreen.findPreference(CATEGORY_KEYS);
         if (!CrUtils.deviceSupportsFlashLight(getContext())) {
-            Preference toRemove = (Preference) prefSet.findPreference(TORCH_POWER_BUTTON_GESTURE);
+            Preference toRemove = (Preference) prefScreen.findPreference(TORCH_POWER_BUTTON_GESTURE);
             if (toRemove != null) {
                 keysCategory.removePreference(toRemove);
             }
@@ -85,6 +105,70 @@ public class Buttons extends CustomSettingsPreferenceFragment implements Prefere
             mTorchPowerButton = (ListPreference) findPreference(TORCH_POWER_BUTTON_GESTURE);
             mTorchPowerButton.setOnPreferenceChangeListener(this);
         }
+
+        final boolean needsNavbar = ActionUtils.hasNavbarByDefault(getActivity());
+        final PreferenceCategory hwkeyCat = (PreferenceCategory) prefScreen
+                .findPreference(CATEGORY_HWKEY);
+
+        // bits for hardware keys present on device
+        final int deviceKeys = getResources()
+                .getInteger(com.android.internal.R.integer.config_deviceHardwareKeys);
+
+        // read bits for present hardware keys
+        final boolean hasHomeKey = (deviceKeys & KEY_MASK_HOME) != 0;
+        final boolean hasBackKey = (deviceKeys & KEY_MASK_BACK) != 0;
+        final boolean hasMenuKey = (deviceKeys & KEY_MASK_MENU) != 0;
+        final boolean hasAssistKey = (deviceKeys & KEY_MASK_ASSIST) != 0;
+        final boolean hasAppSwitchKey = (deviceKeys & KEY_MASK_APP_SWITCH) != 0;
+
+        // load categories and init/remove preferences based on device
+        // configuration
+        final PreferenceCategory backCategory = (PreferenceCategory) prefScreen
+                .findPreference(CATEGORY_BACK);
+        final PreferenceCategory homeCategory = (PreferenceCategory) prefScreen
+                .findPreference(CATEGORY_HOME);
+        final PreferenceCategory menuCategory = (PreferenceCategory) prefScreen
+                .findPreference(CATEGORY_MENU);
+        final PreferenceCategory assistCategory = (PreferenceCategory) prefScreen
+                .findPreference(CATEGORY_ASSIST);
+        final PreferenceCategory appSwitchCategory = (PreferenceCategory) prefScreen
+                .findPreference(CATEGORY_APPSWITCH);
+
+        // back key
+        if (!hasBackKey) {
+            prefScreen.removePreference(backCategory);
+        }
+
+        // home key
+        if (!hasHomeKey) {
+            prefScreen.removePreference(homeCategory);
+        }
+
+        // App switch key (recents)
+        if (!hasAppSwitchKey) {
+            prefScreen.removePreference(appSwitchCategory);
+        }
+
+        // menu key
+        if (!hasMenuKey) {
+            prefScreen.removePreference(menuCategory);
+        }
+
+        // search/assist key
+        if (!hasAssistKey) {
+            prefScreen.removePreference(assistCategory);
+        }
+
+        // let super know we can load ActionPreferences
+        onPreferenceScreenLoaded(ActionConstants.getDefaults(ActionConstants.HWKEYS));
+
+        // load preferences first
+        setActionPreferencesEnabled(true);
+    }
+
+    @Override
+    protected boolean usesExtendedActionsList() {
+        return true;
     }
 
     @Override
